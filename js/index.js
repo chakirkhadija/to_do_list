a2159bd7bd1c7d170b156a806e4cb2eba43b0d58
chakir khadija <chakir.khadija.pro@gamil.com>
2022-01-25 13:19:19 +0100

    my first commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 /*Ajouter*/
 document.getElementsById("btnadd").addEventListener("click",add)
 function add(){
-    var task = document.getElementById("write").textContent = 
-    
+    var task = document.getElementById("write").textContent.trim()
+    if(task!=""){
+        document.getElementById("ulgroupe").innerHTML+='<div class="col-lg-10 offset-lg-1"><ul class="list.group"><li class="list-group-item"><span>'+task+'</span><div class="icons"><i class="fas fa-trash-alt trash"></i><i class="fas fa-user-edit edit"></i></div></li></ul></div>'
+    }
 
 }
 /*Supprimer*/
@@ -26,4 +28,17 @@ for(var i=0;i<list.length;i++){
 function edittask(){
     this.children[0].setAttribute("contenteditable","");
 }
-
+/*Supprimer*/
+document.getElementsById("search").addEventListener("keyup",chercher)
+function chercher(){
+    var cle = this.value
+    for(var i=0; i<list.length; i++){
+        if(!list[i].children[0].textContent.trim().toUpperCase().includes(cle.toUpperCase())){
+            list[i].style.display="none"
+        }
+        else{
+            list[i].style.display="block"
+        }
+        
+    }
+}
