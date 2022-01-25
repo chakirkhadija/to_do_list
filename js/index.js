67e1b292a269bc558a3951ec8c084d99a84df9ae
chakir khadija <chakir.khadija.pro@gamil.com>
2022-01-25 13:00:18 +0100

    t ok

--- js/index.js
+++ js/index.js
@@ -1,10 +1,29 @@
+/*Ajouter*/
+document.getElementsById("btnadd").addEventListener("click",add)
+function add(){
+    var task = document.getElementById("write").textContent = 
+    
+
+}
+/*Supprimer*/
 var del = document.getElementsByClassName("fa-trash-alt");
 for(var i=0;i<del.length;i++){
     del[i].addEventListener("click",deletetask);
 }
+
 function deletetask(){
     var confirmer = confirm("are you sure !!!");
     if(confirmer){
         this.parentElement.parentElement.remove();
     }
 }
+/*Modifier*/
+var list = document.getElementsByTagName("li");
+for(var i=0;i<list.length;i++){
+    list[i].addEventListener("dblclick",edittask);
+}
+
+function edittask(){
+    this.children[0].setAttribute("contenteditable","");
+}
+
